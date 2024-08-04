a360f0810200237cb63545b861eb8bcfa60f7567
koumura <masaki632d@gmail.com>
2024-08-04 23:28:55 +0900

    fix: MobileMenu data

--- src/components/Navbar/MobileMenu/constants/data.ts
+++ src/components/Navbar/MobileMenu/constants/data.ts
@@ -3,20 +3,20 @@ import { HiBeaker, HiBookmarkAlt, HiUser } from 'react-icons/hi'
 export const data = [
   {
     id: 1,
+    title: 'Home',
+    href: '/',
+    icon: HiBookmarkAlt,
+  },
+  {
+    id: 2,
     title: 'About',
     href: '/about',
     icon: HiUser,
   },
   {
-    id: 2,
+    id: 3,
     title: 'Projects',
     href: '/projects',
     icon: HiBeaker,
-  },
-  {
-    id: 3,
-    title: 'Blog',
-    href: '/blog',
-    icon: HiBookmarkAlt,
   },
 ]
--- src/components/NotFoundComponent/index.tsx
+++ src/components/NotFoundComponent/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-
 import duckImage from 'public/searching-duck.gif'
 
 type props = {
