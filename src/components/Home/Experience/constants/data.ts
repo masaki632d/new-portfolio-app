bfbb1e0db1065e417806186f328e559a98bb8c5d
koumura <masaki632d@gmail.com>
2024-08-22 18:20:39 +0900

    fix: br tag, text

--- src/components/Home/Experience/constants/data.ts
+++ src/components/Home/Experience/constants/data.ts
@@ -11,8 +11,8 @@ export const data = [
     startDate: '2019-10',
     endDate: null,
     description: [
-      '<b>概要：</b> 複数の金融系Webメディア の フロントエンド開発 を主に担当（アフィリエイト - SEO / PPC）。その他、Webデザイン・UI/UX / CVR改善・デザインシステム導入・ブランド構築とプロダクトへの反映・リプレイス・保守/運用 など。',
-      '<b>得た経験：</b> 仕事を通して、様々なモダンフロントエンド技術を経験（Next.js・React・TypeScript・TailwindCSS・Storybook・VRTなど）。また、Designer職能内での小規模なフロントエンド開発チームでリーダーを担当し、メンバー管理・PJのリード を経験しました。',
+      '<b>概要：</b> 複数の金融系Webメディア の フロントエンド開発 を主に担当（アフィリエイト - SEO / PPC）。<br>その他、Webデザイン・UI/UX / CVR改善・デザインシステム導入・ブランド構築とプロダクトへの反映・リプレイス・保守/運用 など。',
+      '<b>得た経験：</b> 仕事を通して、様々なモダンフロントエンド技術を経験（Next.js・React・TypeScript・TailwindCSS・Storybook・VRTなど）。<br>また、Designer組織内での小規模なフロントエンド開発チームでリーダーを担当し、メンバー管理・PJのリード を経験しました。',
       '<b>技術スタック：</b> Next.js / React / TypeScript / JavaScript / TailwindCSS / CSS Modules / Emotion / HTML / CSS / Sass / Storybook / VRT / Jest',
       '<b>周辺ツール：</b> Docker / Git / GitLab / Google Analytics 4 / Google Search Console',
       '<b>デザインツール：</b> Figma / Photoshop / Illustrator / XD',
@@ -59,7 +59,7 @@ export const data = [
     startDate: '2014-10',
     endDate: '2017-01',
     description: [
-      '<b>概要：</b> 複数の医療系求人サイトのWeb制作を担当（社内ディレクション・企画・デザイン・コーディング）。Webデザイナー / コーダー / ディレクターを兼務し、施策に基づく新規コンテンツ制作や、採用サイトの制作などを担当。',
+      '<b>概要：</b> 複数の医療系求人サイトのWeb制作を担当（デザイン・コーディング・社内ディレクション・施策の企画）。<br>Webデザイナー / コーダー / ディレクターを兼務し、施策に基づく新規コンテンツ制作や、採用サイトの制作などを担当。',
       '<b>得た経験：</b> 仕事を通して、医療系サービス特有のドメイン知識の獲得や、Web制作の流れ（ディレクション/デザイン/コーディング）や、他職能との連携などを経験することができました。',
       '<b>技術スタック：</b> HTML / CSS / Sass / JavaScript / jQuery / PHP',
       '<b>周辺ツール：</b> Wordpress / Git / Subversion',
